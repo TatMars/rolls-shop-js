08776e1d81bc3d2fb54e5ce2da6bc58f123e5d96
TatMars <Heuukem@gmail.com>
2023-02-25 14:09:25 +0300

    added changes

---
+++ js/calcPrice.js
@@ -0,0 +1,33 @@
+function calcCartPriceAndDelivery() {
+  const cartItems = document.querySelectorAll(".cart-item");
+  const priceTotalEl = document.querySelector(".total-price");
+
+  const deliveryCost = document.querySelector(".delivery-cost");
+  const cartDelivery = document.querySelector("[data-cart-delivery]");
+
+  let totalPrice = 0;
+
+  cartItems.forEach(function (item) {
+    const amountEl = item.querySelector("[data-counter]");
+    const priceEl = item.querySelector(".price__currency");
+    const currentPrice =
+      parseInt(amountEl.innerText) * parseInt(priceEl.innerText);
+
+    totalPrice = totalPrice + currentPrice;
+  });
+
+  priceTotalEl.innerText = totalPrice;
+
+  if (totalPrice > 0) {
+    cartDelivery.classList.remove("none");
+  } else {
+    cartDelivery.classList.add("none");
+  }
+  if (totalPrice >= 600) {
+    deliveryCost.classList.add(".free");
+    deliveryCost.innerText = "0р";
+  } else {
+    deliveryCost.classList.remove(".free");
+    deliveryCost.innerText = "250р";
+  }
+}
